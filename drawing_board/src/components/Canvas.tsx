9de26ecd78b48de52992d3599ac4e2e25f0701f9
minyic22 <minyic22@gmail.com>
2023-04-26 23:39:01 +1000

    feat： Add a word input place

--- drawing_board/src/components/Canvas.tsx
+++ drawing_board/src/components/Canvas.tsx
@@ -1,11 +1,13 @@
 import { useCallback, useEffect, useRef, useState } from 'react';
 import axios from "axios";
+import './css/canvas.css'
 
 interface CanvasProps {
     width: number;
     height: number;
     canvas_color: string;
 }
+
 
 type Coordinate = {
     x: number;
@@ -14,11 +16,45 @@ type Coordinate = {
 
 type Stroke = Coordinate[];
 
+type UserInput = {
+    strokes: Stroke[],
+    shape_id: number|null,
+    shape_type: string,
+    shapetype_code: number|null,
+    params_to_vary: number|null
+    param_value: number|null
+}
+
 const Canvas = ({ width, height , canvas_color}: CanvasProps) => {
     const canvasRef = useRef<HTMLCanvasElement>(null);
     const [isPainting, setIsPainting] = useState(false);
     const [mousePosition, setMousePosition] = useState<Coordinate | undefined>(undefined);
     const [strokes, setStrokes] = useState<Stroke[]>([]);
+    const [canvasWidth, setCanvasWidth] = useState(window.innerWidth);
+    const [canvasHeight, setCanvasHeight] = useState(window.innerHeight - 100);
+    const [inputValue, setInputValue] = useState("");
+
+
+    useEffect(() => {
+        const handleResize = () => {
+            setCanvasWidth(window.innerWidth);
+            setCanvasHeight(window.innerHeight - 100);
+        };
+        window.addEventListener("resize", handleResize);
+        return () => window.removeEventListener("resize", handleResize);
+    }, []);
+
+    useEffect(() => {
+        const canvas = canvasRef.current;
+        if (canvas===null) return;
+        // const ctx = canvas.getContext("2d");
+        // if (ctx===null) return;
+        canvas.width = canvasWidth;
+        canvas.height = canvasHeight;
+        // ctx.fillStyle = canvas_color;
+        // ctx.fillRect(0, 0, canvasWidth, canvasHeight);
+        clearCanvas()  // clear drawing when resized, if needed save to redraw to keep drawing
+    }, [canvasWidth, canvasHeight]);
 
     const startPaint = useCallback((event: MouseEvent) => {
         const coordinates = getCoordinates(event);
@@ -130,9 +166,17 @@ const Canvas = ({ width, height , canvas_color}: CanvasProps) => {
     };
 
     const sendStrokesToBackend = async () => {
-        console.log(strokes)
+        let user_input:UserInput = {
+            strokes: strokes,
+            shape_id: null,
+            shape_type: inputValue,
+            shapetype_code: null,
+            params_to_vary: null,
+            param_value: null,
+        }
+        console.log(user_input)
         try {
-            const response = await axios.post('http://172.25.97.82:5000/send_strokes', strokes);
+            const response = await axios.post('http://172.25.97.82:5000/send_strokes', user_input);
 
             console.log(response.data);
         } catch (error) {
@@ -142,11 +186,21 @@ const Canvas = ({ width, height , canvas_color}: CanvasProps) => {
 
 
     return(
-        <>
-            <button type="button" className="btn btn-info" onClick={sendStrokesToBackend}>Get Shape</button>
-            <button type="button" className="btn btn-warning" onClick={clearCanvas}>Clear Canvas</button>
+        <div id="canvas_container">
+            <span>
+                <button type="button" className="btn btn-info" onClick={sendStrokesToBackend}>Get Shape</button>
+                <button type="button" className="btn btn-warning" onClick={clearCanvas}>Clear Canvas</button>
+            </span>
+            <input
+                type="text"
+                className="form-control"
+                placeholder="Input Word"
+                style={{ width: 300 }}
+                value={inputValue}
+                onChange={(event) => setInputValue(event.target.value)}
+            />
             <canvas ref={canvasRef} height={height} width={width} style={{backgroundColor: canvas_color}}/>;
-        </>
+        </div>
     )
 };
 
